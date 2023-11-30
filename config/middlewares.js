f310764cdaac8f2232526af9895c29e3c2e9558b
FooQoo <horseshoes.buzz.17@gmail.com>
2023-11-30 11:02:40 +0900

    middleware追加

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,5 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -9,4 +8,39 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'https://market-assets.strapi.io',
+            /**
+             * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
+             * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
+             */
+            `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            /**
+             * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
+             * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
+             */
+            `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
