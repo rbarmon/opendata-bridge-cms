1c3b6ce180d4db54f5cd489b5c5e5d70e598ec0d
FooQoo <horseshoes.buzz.17@gmail.com>
2023-12-31 10:34:42 +0900

    feat: レシピコンポーネントを追加する

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -14,6 +14,7 @@ import customisations from "../../src/admin/app.js";
 
 renderAdmin(document.getElementById("strapi"), {
   customisations,
+
   plugins: {
     "strapi-cloud": strapiCloud,
     graphql: graphql,
--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,22 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ProjectRecipe extends Schema.Component {
+  collectionName: 'components_project_recipes';
+  info: {
+    displayName: 'recipe';
+    icon: 'file';
+    description: '';
+  };
+  attributes: {
+    prompt: Attribute.Text;
+    script: Attribute.Text;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'project.recipe': ProjectRecipe;
+    }
+  }
 }
